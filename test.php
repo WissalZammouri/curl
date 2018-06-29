186438558c84d1c596ce405c691d732f88a8dc83
wissal <wiss9605@gmail.com>
2018-06-29 17:59:30 +0100

    test de plusieurs urls

--- test.php
+++ test.php
@@ -1,10 +1,25 @@
 <?php
-$url=$_POST["url"];
-$init=curl_init($url);
-curl_setopt($init,CURLOPT_URL,$url);
-curl_setopt($init, CURLOPT_RETURNTRANSFER, 1);
-$result=curl_exec($init);
-$info=curl_getinfo($init);
-echo json_encode($info);
+//variables
+$urlString=$_POST["urls"];
+$urls=explode(";",$urlString);
+
+
+foreach ($urls as $url) 
+{
+    //initialisation
+    $init=curl_init($url);
+    curl_setopt($init,CURLOPT_URL,$url);
+    curl_setopt($init, CURLOPT_RETURNTRANSFER, 1);
+    
+    //execute
+    $result=curl_exec($init);
+    
+    //get HTTP code & time
+    $code=curl_getinfo($init, CURLINFO_HTTP_CODE);
+    $time=curl_getinfo($init,CURLINFO_TOTAL_TIME);
+
+    //display results
+    include('resultPage.html');
+}
 curl_close($init);
 ?>
